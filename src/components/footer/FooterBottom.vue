10a00327015a2e14791a9f171afde9964c8cf9ed
FrancescaLoria <francesca.loria90@hotmail.it>
2023-04-28 10:48:27 +0200

    footer bottom

--- src/components/footer/FooterBottom.vue
+++ src/components/footer/FooterBottom.vue
@@ -9,7 +9,30 @@ export default {
 </script>
 
 <template>
-  <h1>ciao</h1>
+  <div class="footer-bottom">
+    <div class="ms_container">
+      <div class="d-flex justify-content-between">
+        <div class="text-left">
+          Enjoy the low price. We are tracking any intention of piracy
+        </div>
+        <div class="text-right d-flex">
+          <div>&copy;2020 NEXGEN is Proudly Powered by</div>
+          <span class="ms-1">Codings</span>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
-<style scoped lang=""></style>
+<style scoped lang="scss">
+.footer-bottom {
+  padding: 30px 0;
+  background-color: #0d0d11;
+  font-size: 12px;
+  color: white;
+
+  span {
+    color: #058283;
+  }
+}
+</style>
